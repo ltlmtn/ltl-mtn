482fefb4d7b125ca43da82954ad15007a5a43e74
Elliot Toman <web@asubtleweb.com>
2025-03-17 12:07:08 -0400

    Homepage video updates

--- wp-content/themes/ltl-mtn-2024/snippets/homepage-anchors.php
+++ wp-content/themes/ltl-mtn-2024/snippets/homepage-anchors.php
@@ -1,8 +1,8 @@
+<div class="menu-toggle">
+    <div class="menu-open"><ion-icon name="menu"></ion-icon></div>
+    <div class="menu-close"><ion-icon name="close"></ion-icon></div>
+</div>
 <nav class="anchor-menu">
-    <div class="menu-toggle">
-        <div class="menu-open"><ion-icon name="menu"></ion-icon></div>
-        <div class="menu-close"><ion-icon name="close"></ion-icon></div>
-    </div>
     <?php wp_nav_menu( array( 
         'theme_location' => 'homepage_anchors',
         'container' => 'nav',
