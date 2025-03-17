2616ff6e9f7fc0d6891484824781f2bd201bdb07
Elliot Toman <web@asubtleweb.com>
2025-03-17 14:37:05 -0400

    Homepage Updates

--- wp-content/themes/ltl-mtn-2024/snippets/homepage-anchors.php
+++ wp-content/themes/ltl-mtn-2024/snippets/homepage-anchors.php
@@ -1,12 +1,15 @@
-<div class="menu-toggle">
-    <div class="menu-open"><ion-icon name="menu"></ion-icon></div>
-    <div class="menu-close"><ion-icon name="close"></ion-icon></div>
+<div class="anchor-menu-container">
+    <input type="checkbox" id="anchor-menu-toggle" class="anchor-menu-toggle-checkbox">
+    <label class="anchor-menu-toggle" for="anchor-menu-toggle">
+        <div class="anchor-menu-open"><ion-icon name="menu"></ion-icon></div>
+        <div class="anchor-menu-close"><ion-icon name="close"></ion-icon></div>
+    </label>
+    <nav class="anchor-menu">
+        <?php wp_nav_menu( array( 
+            'theme_location' => 'homepage_anchors',
+            'container' => 'nav',
+            'container_class' => 'main-navigation',
+            'menu_class' => 'nav-menu',
+        ) ); ?>
+    </nav>
 </div>
-<nav class="anchor-menu">
-    <?php wp_nav_menu( array( 
-        'theme_location' => 'homepage_anchors',
-        'container' => 'nav',
-        'container_class' => 'main-navigation',
-        'menu_class' => 'nav-menu',
-    ) ); ?>
-</nav>
